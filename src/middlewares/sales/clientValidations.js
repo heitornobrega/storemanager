47657a9e24bade17a7fd63d699d0bd43bc2ef35a
heitornobrega <nobheitor@gmail.com>
2022-09-19 18:05:18 -0300

    fix: requisito 6

--- src/middlewares/sales/clientValidations.js
+++ src/middlewares/sales/clientValidations.js
@@ -1,21 +1,21 @@
-const inputIsValid = async (req, res, next) => {
-  const input = req.body;
-  const test = await input.map(({ productId, quantity }) => {
-    if (!productId) {
-      return res.status(400).json({ message: '"productId" is required' });
-    }
-    if (!quantity) {
-      return res.status(400).json({ message: '"quantity" is required' });
-    }
-    if (quantity < 0 || quantity === 0) {
-      return res
-        .status(422)
-        .json({ message: '"quantity" must be greater than or equal to 1' });
-    }
-     return [];
-  });
-  const sdf = await Promise.all(test);
-  const val = sdf.every((el) => el.length === 0);
-  if (val) return next();
-};
-module.exports = { inputIsValid };
+// const inputIsValid = async (req, res, next) => {
+//   const input = req.body;
+//   const test = await input.map(({ productId, quantity }) => {
+//     if (!productId) {
+//       return res.status(400).json({ message: '"productId" is required' });
+//     }
+//     if (!quantity) {
+//       return res.status(400).json({ message: '"quantity" is required' });
+//     }
+//     if (quantity < 0 || quantity === 0) {
+//       return res
+//         .status(422)
+//         .json({ message: '"quantity" must be greater than or equal to 1' });
+//     }
+//      return [];
+//   });
+//   const sdf = await Promise.all(test);
+//   const val = sdf.every((el) => el.length === 0);
+//   if (val) return next();
+// };
+// module.exports = { inputIsValid };
